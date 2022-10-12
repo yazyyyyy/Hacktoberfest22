669f4167a5df6b11ae56c7e2d8da3bb5dc6b362b
Hritik Soni <56077574+iamhritik55@users.noreply.github.com>
2022-10-12 21:02:09 +0530

    Create fibonacci using recursion.cpp

---
+++ C++/fibonacci using recursion.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h> 
+int fibonacci(int);
+ 
+ 
+int main()
+{    
+    int terms;
+    
+    printf("Enter terms: ");
+    scanf("%d", &terms);       
+    
+    for(int n = 0; n < terms; n++)
+    {
+        printf("%d ", fibonacci(n));
+    }
+    
+    return 0; 
+}
+ 
+ 
+int fibonacci(int num)
+{    
+    
+    
+    if(num == 0 || num == 1)
+    {
+        return num;
+    }
+    
+    else
+    {
+    
+        return fibonacci(num-1) + fibonacci(num-2);
+    }
+    
+}
