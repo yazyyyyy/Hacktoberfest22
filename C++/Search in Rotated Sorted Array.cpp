f19585f63a5f7712a21ee75098ba30960b749afb
HARSH GUPTA <87202102+HarshGupta74@users.noreply.github.com>
2022-10-16 14:34:52 +0530

    Create Search in Rotated Sorted Array.cpp
    Added solution to leetcode problem no. 33

---
+++ C++/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,30 @@
+#include<iostream.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0,e=nums.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>=nums[s]){
+                if(target<nums[mid] && target>=nums[s]){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }else{
+                if(target>nums[mid] && target<=nums[e]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
