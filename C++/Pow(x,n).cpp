dd3524846d5a269be93696340411e530c899000e
BhawinAmeta <107951583+bhawinameta@users.noreply.github.com>
2022-10-19 19:44:42 +0530

    Create Pow(x,n).cpp
    solution to leetcode problem 50

---
+++ C++/Pow(x,n).cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) {
+       if(n==0)
+            return 1;
+        
+        if(n<0){
+            x = 1/x;
+            n = abs(n);
+        }
+        if(n%2==0){
+             return myPow(x*x,n/2);
+        }
+        
+        else return x*myPow(x*x,n/2);     
+    }
+
+};
