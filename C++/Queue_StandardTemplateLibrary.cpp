82d3078c3954e0b278a5c999058ff39ff882991d
imred4 <116151693+imred4@users.noreply.github.com>
2022-10-19 14:53:37 +0530

    Create Queue_StandardTemplateLibrary.cpp

---
+++ C++/Queue_StandardTemplateLibrary.cpp
@@ -0,0 +1,48 @@
+// CPP code to illustrate Queue in 
+// Standard Template Library (STL)
+#include <iostream>
+#include <queue>
+  
+using namespace std;
+  
+// Print the queue
+void showq(queue<int> gq)
+{
+    queue<int> g = gq;
+    while (!g.empty()) {
+        cout << '\t' << g.front();
+        g.pop();
+    }
+    cout << '\n';
+}
+  
+// Driver Code
+int main()
+{
+    queue<int> gquiz;
+    gquiz.push(10);
+    gquiz.push(20);
+    gquiz.push(30);
+  
+    cout << "The queue gquiz is : ";
+    showq(gquiz);
+  
+    cout << "\ngquiz.size() : " << gquiz.size();
+    cout << "\ngquiz.front() : " << gquiz.front();
+    cout << "\ngquiz.back() : " << gquiz.back();
+  
+    cout << "\ngquiz.pop() : ";
+    gquiz.pop();
+    showq(gquiz);
+  
+    return 0;
+}
+/*
+Output
+The queue gquiz is :     10    20    30
+
+gquiz.size() : 3
+gquiz.front() : 10
+gquiz.back() : 30
+gquiz.pop() :     20    30
+*/
