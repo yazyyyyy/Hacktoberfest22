714c39d7818a73b137a63e4be72496f11673683d
Sakshi Mishra <84084388+sam2611@users.noreply.github.com>
2022-10-24 23:26:35 +0530

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,14 @@
+// Linear Search in C++
+
+#include <iostream>
+using namespace std;
+
+int search(int array[], int n, int x)
+{
+
+	// Going through array sequencially
+	for (int i = 0; i < n; i++)
+		if (array[i] == x)
+			return i;
+	return -1;
+}
